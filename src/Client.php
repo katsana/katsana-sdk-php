cc4661a8bb3388a2126e06b9dba2c31dc5c8c288
crynobone <crynobone@gmail.com>
2016-08-16 09:51:35 +0800

    Add draft Client.
    Signed-off-by: crynobone <crynobone@gmail.com>

--- src/Client.php
+++ src/Client.php
@@ -2,7 +2,69 @@
 
 namespace Katsana\Sdk;
 
+use Http\Discovery\HttpClientDiscovery;
+use Http\Discovery\MessageFactoryDiscovery;
+use Http\Client\Common\HttpMethodsClient as HttpClient;
+
 class Client
 {
-    //
+    /**
+     * Http Client instance.
+     *
+     * @var \Http\Client\Common\HttpMethodsClient
+     */
+    protected $http;
+
+    /**
+     * API Key.
+     *
+     * @var string
+     */
+    protected $apiKey;
+
+    /**
+     * API Secret.
+     *
+     * @var string
+     */
+    protected $apiSecret;
+
+    /**
+     * API endpoint.
+     *
+     * @var string
+     */
+    protected $apiEndpoint = 'https://api.katsana.com';
+
+    /**
+     * Construct a new Client.
+     *
+     * @param \Http\Client\Common\HttpMethodsClient  $http
+     * @param string  $apiKey
+     * @param string  $apiSecret
+     */
+    public function __construct(HttpClient $http, $apiKey, $apiSecret)
+    {
+        $this->http   = $http;
+        $this->apiKey = $apiKey;
+        $this->apiSecret = $apiSecret;
+    }
+
+    /**
+     * Make a client.
+     *
+     * @param string  $apiKey
+     * @param string  $apiSecret
+     *
+     * @return $this
+     */
+    public static function make($apiKey, $apiSecret)
+    {
+        $client = new HttpClient(
+            HttpClientDiscovery::find(),
+            MessageFactoryDiscovery::find()
+        );
+
+        return new static($client, $apiKey, $apiSecret);
+    }
 }
