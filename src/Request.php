77e42e3688bc57af13615f5c72083da171b21970
crynobone <crynobone@gmail.com>
2016-08-21 00:42:54 +0800

    Add Request class.
    Signed-off-by: crynobone <crynobone@gmail.com>

---
+++ src/Request.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace Katsana\Sdk;
+
+use GuzzleHttp\Psr7\Uri;
+
+abstract class Request
+{
+    /**
+     * Version namespace.
+     *
+     * @var string
+     */
+    protected $version;
+
+    /**
+     * The Billplz client.
+     *
+     * @var \Katsana\Sdk\Client
+     */
+    protected $client;
+
+    /**
+     * Get API endpoint.
+     *
+     * @param  string  $name
+     * @param  array  $headers
+     *
+     * @return array
+     */
+    protected function endpoint($name, array $headers = [])
+    {
+        $domain = $this->client->getApiEndpoint();
+
+        $uri = (new Uri(sprintf('%s/%s/%s', $domain, $name)));
+        $headers['Accept'] = "application/vnd.katsana.{$this->version}+json";
+
+        return [$uri, $headers];
+    }
+}
