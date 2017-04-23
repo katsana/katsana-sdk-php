a03bb39af395384d5887987819e7e67d8d8b4992
crynobone <crynobone@gmail.com>
2017-04-23 11:21:30 +0800

    Update sharing API.
    Signed-off-by: crynobone <crynobone@gmail.com>

--- src/One/Vehicles/Sharing.php
+++ src/One/Vehicles/Sharing.php
@@ -21,4 +21,60 @@ class Sharing extends Request
             $this->getApiHeaders()
         );
     }
+
+    /**
+     * Create a new share.
+     *
+     * @param  int  $vehicleId
+     * @param  string|null  $description
+     * @param  string|null  $duration
+     *
+     * @return \Katsana\Sdk\Response
+     */
+    public function store($vehicleId, $description, $duration)
+    {
+        return $this->send(
+            'POST',
+            "vehicles/{$vehicleId}/sharing",
+            $this->getApiHeaders(),
+            $this->mergeApiBody(compact('description', 'duration'))
+        );
+    }
+
+    /**
+     * Update an existing share.
+     *
+     * @param  int  $vehicleId
+     * @param  int  $sharingId
+     * @param  string|null  $description
+     * @param  string|null  $duration
+     *
+     * @return \Katsana\Sdk\Response
+     */
+    public function update($vehicleId, $sharingId, $description, $duration)
+    {
+        return $this->send(
+            'PATCH',
+            "vehicles/{$vehicleId}/sharing/{$sharingId}",
+            $this->getApiHeaders(),
+            $this->mergeApiBody(compact('description', 'duration'))
+        );
+    }
+
+    /**
+     * Delete an existing share.
+     *
+     * @param  int  $vehicleId
+     * @param  int  $sharingId
+     *
+     * @return \Katsana\Sdk\Response
+     */
+    public function destroy($vehicleId, $sharingId)
+    {
+        return $this->send(
+            'DELETE',
+            "vehicles/{$vehicleId}/sharing/{$sharingId}",
+            $this->getApiHeaders()
+        );
+    }
 }
