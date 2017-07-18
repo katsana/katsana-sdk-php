9dda791a8d9ea90af62966860931c654149e7968
crynobone <crynobone@gmail.com>
2017-07-18 15:23:42 +0800

    Add duration.
    Signed-off-by: crynobone <crynobone@gmail.com>

---
+++ src/One/Vehicles/Track.php
@@ -0,0 +1,78 @@
+<?php
+
+namespace Katsana\Sdk\One\Vehicles;
+
+use Katsana\Sdk\One\Request;
+
+class Track extends Request
+{
+    /**
+     * Get travel for today.
+     *
+     * @param  int  $id
+     *
+     * @return \Katsana\Sdk\Response
+     */
+    public function today($id)
+    {
+        return $this->send(
+            'GET',
+            "vehicles/{$id}/tracks/today",
+            $this->getApiHeaders()
+        );
+    }
+
+    /**
+     * Get travel for yesterday.
+     *
+     * @param  int  $id
+     *
+     * @return \Katsana\Sdk\Response
+     */
+    public function yesterday($id)
+    {
+        return $this->send(
+            'GET',
+            "vehicles/{$id}/tracks/yesterday",
+            $this->getApiHeaders()
+        );
+    }
+
+    /**
+     * Get travel for the date.
+     *
+     * @param  int  $id
+     * @param  int  $year
+     * @param  int  $month
+     * @param  int  $day
+     *
+     * @return \Katsana\Sdk\Response
+     */
+    public function date($id, $year, $month = 1, $day = 1)
+    {
+        return $this->send(
+            'GET',
+            "vehicles/{$id}/tracks/{$year}/{$month}/{$day}",
+            $this->getApiHeaders()
+        );
+    }
+
+    /**
+     * Get travel for a duration.
+     *
+     * @param  int  $id
+     * @param  string  $start
+     * @param  string  $end
+     *
+     * @return \Katsana\Sdk\Response
+     */
+    public function duration($id, $start, $end)
+    {
+        return $this->send(
+            'GET',
+            "vehicles/{$id}/tracks/duration",
+            $this->getApiHeaders(),
+            compact('start', 'end')
+        );
+    }
+}
