239efe5d064ddc8265f254b87281820c0105d255
Mior Muhammad Zaki <crynobone@gmail.com>
2018-03-12 20:34:25 +0800

    Implement query builder.
    Signed-off-by: Mior Muhammad Zaki <crynobone@gmail.com>

---
+++ src/Query.php
@@ -0,0 +1,127 @@
+<?php
+
+namespace Katsana\Sdk;
+
+class Query
+{
+    /**
+     * Set requested page.
+     *
+     * @var int|null
+     */
+    protected $page;
+
+    /**
+     * Includes data.
+     *
+     * @var array
+     */
+    protected $includes = [];
+
+    /**
+     * Excludes data.
+     *
+     * @var array
+     */
+    protected $excludes = [];
+
+    /**
+     * Custom data.
+     *
+     * @var array
+     */
+    protected $customs = [];
+
+    /**
+     * Make Query builder.
+     *
+     * @return static
+     */
+    public static function make()
+    {
+        return new static();
+    }
+
+    /**
+     * Set includes data.
+     *
+     * @param  array  $includes
+     * @return $this
+     */
+    public function includes($includes)
+    {
+        $includes = is_array($includes) ? $includes : func_get_args();
+
+        $this->includes = $includes;
+
+        return $this;
+    }
+
+    /**
+     * Set excludes data.
+     *
+     * @param  array  $excludes
+     * @return $this
+     */
+    public function excludes($excludes)
+    {
+        $excludes = is_array($excludes) ? $excludes : func_get_args();
+
+        $this->excludes = $excludes;
+
+        return $this;
+    }
+
+    /**
+     * Set custom data.
+     *
+     * @param  string  $name
+     * @param  mixed  $value
+     * @return $this
+     */
+    public function with($name, $value)
+    {
+        $this->customs[$name] = $value;
+
+        return $this;
+    }
+
+    /**
+     * Set current page.
+     *
+     * @param  int|null $page
+     * @return $this
+     */
+    public function forPage($page = null)
+    {
+        $this->page = $page;
+
+        return $this;
+    }
+
+    /**
+     * Build query string.
+     *
+     * @param  callable|null $callback
+     * @return array
+     */
+    public function build(callable $callback = null)
+    {
+        $data = [
+            'includes' => implode(',', $this->includes),
+            'excludes' => implode(',', $this->excludes),
+        ];
+
+        if (is_int($this->page) && $this->page > 0) {
+            $data['page'] = $this->page;
+        }
+
+        if (is_null($callback)) {
+            $callback = function($data, $customs) {
+                return array_merge($customs, $data);
+            };
+        }
+
+        return call_user_func($callback, $data, $this->customs);
+    }
+}
---
+++ tests/QueryTest.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace Katsana\Sdk\Tests;
+
+use Katsana\Sdk\Query;
+
+class QueryTest extends TestCase
+{
+    /** @test */
+    public function it_can_be_initiated()
+    {
+        $query = Query::make()
+                    ->includes('drivemark', 'speed')
+                    ->excludes('driver')
+                    ->forPage(5);
+
+        $this->assertSame([
+            'includes' => 'drivemark,speed',
+            'excludes' => 'driver',
+            'page' => 5,
+        ], $query->build());
+    }
+
+    /** @test */
+    public function it_can_be_initiated_with_customs()
+    {
+        $query = Query::make()
+                    ->includes('drivemark', 'speed')
+                    ->excludes('driver')
+                    ->with('take', 1000)
+                    ->forPage(5);
+
+        $this->assertSame([
+            'take' => 1000,
+            'includes' => 'drivemark,speed',
+            'excludes' => 'driver',
+            'page' => 5,
+        ], $query->build());
+    }
+
+    /** @test */
+    public function it_can_be_build_with_custom_callback()
+    {
+        $query = Query::make()
+                    ->includes('drivemark', 'speed')
+                    ->excludes('driver')
+                    ->with('take', 1000)
+                    ->forPage(5);
+
+        $this->assertSame([
+            'content' => [
+                'take' => 1000,
+                'includes' => 'drivemark,speed',
+                'excludes' => 'driver',
+                'page' => 5,
+            ],
+        ], $query->build(function ($data, $customs) {
+            return ['content' => array_merge($customs, $data)];
+        }));
+    }
+}
