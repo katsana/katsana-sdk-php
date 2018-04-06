ad894778d060f45a58681a574e5b0557d4a22122
Mior Muhammad Zaki <crynobone@gmail.com>
2018-04-06 16:17:13 +0800

    Improves tests.
    Signed-off-by: Mior Muhammad Zaki <crynobone@gmail.com>

--- tests/One/VehiclesTest.php
+++ tests/One/VehiclesTest.php
@@ -23,7 +23,6 @@ class VehicleTest extends TestCase
                         ->expectEndpointIs('https://api.katsana.com/vehicles')
                         ->shouldResponseWith(200, '{"devices":[{"id":105,"user_id":1,"imei":"356173063386671","description":"Peugeot 308","vehicle_number":"WXG 3365","current":{"latitude":3.0093493,"longitude":101.5976447,"speed":0,"state":"idle","ignition":false,"voltage":12485,"gsm":3,"tracked_at":"2017-01-09 13:31:51"},"avatar":"https://my.katsana.com/pictures/device-105/04375b22-d454-11e5-8724-f23c9126a0cc.thumb.png","marker":"https://my.katsana.com/pictures/device-105/04375b22-d454-11e5-8724-f23c9126a0cc.marker.png","today_max_speed":60.47518,"odometer":124277,"ends_at":"2020-08-30 16:00:00","timezone":"Asia/Kuala_Lumpur"}]}');
 
-
         $response = $this->makeClientWithAccessToken($faker)
                         ->uses('Vehicles')
                         ->all();
@@ -35,6 +34,7 @@ class VehicleTest extends TestCase
         $this->assertSame('Peugeot 308', $data['devices'][0]['description']);
         $this->assertSame('WXG 3365', $data['devices'][0]['vehicle_number']);
     }
+
     /**
      * @test
      * @expectedException \Katsana\Sdk\Exceptions\MissingAccessToken
@@ -57,21 +57,21 @@ class VehicleTest extends TestCase
 
         $faker = Faker::create()
                         ->call('GET', $headers)
-                        ->expectEndpointIs('https://api.katsana.com/vehicles')
-                        ->shouldResponseWith(200, '{"devices":[{"id":105,"user_id":1,"imei":"356173063386671","description":"Peugeot 308","vehicle_number":"WXG 3365","current":{"latitude":3.0093493,"longitude":101.5976447,"speed":0,"state":"idle","ignition":false,"voltage":12485,"gsm":3,"tracked_at":"2017-01-09 13:31:51"},"avatar":"https://my.katsana.com/pictures/device-105/04375b22-d454-11e5-8724-f23c9126a0cc.thumb.png","marker":"https://my.katsana.com/pictures/device-105/04375b22-d454-11e5-8724-f23c9126a0cc.marker.png","today_max_speed":60.47518,"odometer":124277,"ends_at":"2020-08-30 16:00:00","timezone":"Asia/Kuala_Lumpur"}]}');
-
+                        ->expectEndpointIs('https://api.katsana.com/vehicles/105')
+                        ->shouldResponseWith(200, '{"device":{"id":105,"user_id":1,"imei":"356173063386671","description":"Peugeot 308","vehicle_number":"WXG 3365","current":{"latitude":3.0093493,"longitude":101.5976447,"speed":0,"state":"idle","ignition":false,"voltage":12485,"gsm":3,"tracked_at":"2017-01-09 13:31:51"},"avatar":"https://my.katsana.com/pictures/device-105/04375b22-d454-11e5-8724-f23c9126a0cc.thumb.png","marker":"https://my.katsana.com/pictures/device-105/04375b22-d454-11e5-8724-f23c9126a0cc.marker.png","today_max_speed":60.47518,"odometer":124277,"ends_at":"2020-08-30 16:00:00","timezone":"Asia/Kuala_Lumpur"}}');
 
         $response = $this->makeClientWithAccessToken($faker)
                         ->uses('Vehicles')
-                        ->all();
+                        ->get(105);
 
         $data = $response->toArray();
 
         $this->assertInstanceOf(Response::class, $response);
-        $this->assertSame(105, $data['devices'][0]['id']);
-        $this->assertSame('Peugeot 308', $data['devices'][0]['description']);
-        $this->assertSame('WXG 3365', $data['devices'][0]['vehicle_number']);
+        $this->assertSame(105, $data['device']['id']);
+        $this->assertSame('Peugeot 308', $data['device']['description']);
+        $this->assertSame('WXG 3365', $data['device']['vehicle_number']);
     }
+
     /**
      * @test
      * @expectedException \Katsana\Sdk\Exceptions\MissingAccessToken
@@ -82,6 +82,39 @@ class VehicleTest extends TestCase
         $this->makeClient(Faker::create())
                     ->uses('Vehicles')
                     ->get(105);
+    }
+
+    /** @test */
+    public function it_can_show_vehicle_location()
+    {
+        $headers = [
+            'Accept' => 'application/vnd.KATSANA.v1+json',
+            'Authorization' => 'Bearer '.static::ACCESS_TOKEN,
+        ];
+
+        $faker = Faker::create()
+                        ->call('GET', $headers)
+                        ->expectEndpointIs('https://api.katsana.com/vehicles/105/location')
+                        ->shouldResponseWith(200, '{"id":105,"latitude":3.0093493,"longitude":101.5976447,"speed":0,"state":"idle","ignition":false,"voltage":12485,"gsm":3,"tracked_at":"2017-01-09 13:31:51"}');
+
+
+        $response = $this->makeClientWithAccessToken($faker)
+                        ->uses('Vehicles')
+                        ->location(105);
+
+        $data = $response->toArray();
+    }
+
+    /**
+     * @test
+     * @expectedException \Katsana\Sdk\Exceptions\MissingAccessToken
+     * @expectedExceptionMessage Request requires valid access token to be available!
+     */
+    public function it_cant_show_vehicle_location_without_access_token()
+    {
+        $this->makeClient(Faker::create())
+                    ->uses('Vehicles')
+                    ->location(105);
     }
 
     /** @test */
