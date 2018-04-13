1c431dc9b542013c358baae3b81553530bcdccbf
Mior Muhammad Zaki <crynobone@gmail.com>
2018-04-13 08:03:20 +0800

    Add tests for Signature.
    Signed-off-by: Mior Muhammad Zaki <crynobone@gmail.com>

---
+++ tests/SignatureTest.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace Katsana\Sdk\Tests;
+
+use Katsana\Sdk\Response;
+use Katsana\Sdk\Signature;
+use Laravie\Codex\Testing\Faker;
+
+class SignatureTest extends TestCase
+{
+    /** @test */
+    public function it_can_verify_response()
+    {
+        $now = time();
+        $payload = '{"device_id":123,"event":"Device+mobilized"}';
+
+        $response = new Response($this->getMessageWithPayload('secret', $payload, $now));
+
+        $signature = new Signature('secret');
+        $this->assertTrue($signature->verify($response));
+    }
+
+    /** @test */
+    public function it_cant_verify_response_given_invalid_signature()
+    {
+        $now = time();
+        $payload = '{"device_id":123,"event":"Device+mobilized"}';
+
+        $response = new Response($this->getMessageWithPayload('secret!', $payload, $now));
+
+        $signature = new Signature('secret');
+        $this->assertFalse($signature->verify($response));
+    }
+
+    /** @test */
+    public function it_can_verify_response_when_timestamp_exceed_treshold()
+    {
+        $now = time() - 3600;
+        $payload = '{"device_id":123,"event":"Device+mobilized"}';
+
+        $response = new Response($this->getMessageWithPayload('secret', $payload, $now));
+
+        $signature = new Signature('secret');
+        $this->assertFalse($signature->verify($response, 60));
+    }
+
+    /**
+     * Get mocked message with payload.
+     *
+     * @param string $key
+     * @param string $payload
+     * @param int    $timestamp
+     *
+     * @return \Mockery\MockeryInterface
+     */
+    protected function getMessageWithPayload(string $key, string $payload, int $timestamp)
+    {
+        $message = Faker::create()->message();
+        $signature = hash_hmac('sha256', "{$timestamp}.{$payload}", $key);
+
+        $message->shouldReceive('getHeader')->with('HTTP_X_SIGNATURE')->andReturn([
+            "t={$timestamp},v1={$signature}",
+        ])->shouldReceive('getBody')->andReturn($payload);
+
+        return $message;
+    }
+}
