d69c363ed3cacaa1a8ab72f1ea705973c38f8efa
Mior Muhammad Zaki <crynobone@gmail.com>
2018-04-24 16:48:46 +0800

    Add Driver management for fleet.
    Signed-off-by: Mior Muhammad Zaki <crynobone@gmail.com>

---
+++ src/One/Fleets/Driver.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace Katsana\Sdk\One\Fleets;
+
+use Katsana\Sdk\One\Request;
+use Laravie\Codex\Contracts\Response;
+
+class Driver extends Request
+{
+    /**
+     * Create driver for a fleet.
+     *
+     * @param int    $fleetId
+     * @param string $fullname
+     * @param string $identification
+     * @param array  $optional
+     *
+     * @return \Laravie\Codex\Contracts\Response
+     */
+    public function create(int $fleetId, string $fullname, string $identification, array $optional = []): Response
+    {
+        return $this->sendJson(
+            'POST',
+            "fleets/{$fleetId}/drivers",
+            $this->getApiHeaders(),
+            array_merge($optional, compact('fullname', 'identification'))
+        );
+    }
+
+    /**
+     * Create driver for a fleet.
+     *
+     * @param int    $fleetId
+     * @param int    $driverId
+     * @param string $fullname
+     * @param string $identification
+     * @param array  $optional
+     *
+     * @return \Laravie\Codex\Contracts\Response
+     */
+    public function update(int $fleetId, int $driverId, string $fullname, string $identification, array $optional = []): Response
+    {
+        return $this->sendJson(
+            'POST',
+            "fleets/{$fleetId}/drivers/{$driverId}",
+            $this->getApiHeaders(),
+            array_merge($optional, compact('fullname', 'identification'))
+        );
+    }
+}
