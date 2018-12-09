8d6a90bddc31b7352cf10a5403ee14756fa9ad68
Mior Muhammad Zaki <crynobone@gmail.com>
2018-12-09 12:07:12 +0800

    Update deps.
    Signed-off-by: Mior Muhammad Zaki <crynobone@gmail.com>

--- src/Request.php
+++ src/Request.php
@@ -2,7 +2,9 @@
 
 namespace Katsana\Sdk;
 
+use Laravie\Codex\Contracts\Response as ResponseContract;
 use Laravie\Codex\Request as BaseRequest;
+use Psr\Http\Message\ResponseInterface;
 
 abstract class Request extends BaseRequest
 {
@@ -12,6 +14,18 @@ abstract class Request extends BaseRequest
      * @var string
      */
     protected $requestHeaderTimezoneCode = 'UTC';
+
+    /**
+     * Resolve the responder class.
+     *
+     * @param \Psr\Http\Message\ResponseInterface $response
+     *
+     * @return \Laravie\Codex\Contracts\Response
+     */
+    protected function responseWith(ResponseInterface $response): ResponseContract
+    {
+        return new Response($response);
+    }
 
     /**
      * Get API Header.
