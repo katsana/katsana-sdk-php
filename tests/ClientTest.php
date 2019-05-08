d77af7c553149691d57c6b1d2a04da959b10bcfb
Mior Muhammad Zaki <crynobone@gmail.com>
2019-05-08 19:47:30 +0800

    Improve tests.
    Signed-off-by: Mior Muhammad Zaki <crynobone@gmail.com>

--- tests/ClientTest.php
+++ tests/ClientTest.php
@@ -2,12 +2,21 @@
 
 namespace Katsana\Sdk\Tests;
 
+use Mockery as m;
 use Katsana\Sdk\Client;
 use Laravie\Codex\Discovery;
 use Laravie\Codex\Testing\Faker;
 
 class ClientTest extends TestCase
 {
+    /**
+     * Teardown the test environment.
+     */
+    protected function tearDown(): void
+    {
+        m::close();
+    }
+
     /** @test */
     public function it_has_proper_signature()
     {
@@ -48,5 +57,38 @@ class ClientTest extends TestCase
         $this->assertNull($client->getClientId());
         $this->assertNull($client->getClientSecret());
         $this->assertSame('AckfSECXIvnK5r28GVIWUAxmbBSjTsmF', $client->getAccessToken());
+    }
+
+    /** @test */
+    public function it_can_use_the_default_timezone_on_request()
+    {
+        $faker = Faker::create();
+
+        Discovery::override($faker->http());
+
+        $client = Client::personal('AckfSECXIvnK5r28GVIWUAxmbBSjTsmF');
+
+        $request = m::mock('Laravie\Codex\Contracts\Request');
+        $request->shouldReceive('setClient')->once()->with($client)->andReturnNull()
+            ->shouldReceive('onTimeZone')->once()->with('UTC')->andReturnNull();
+
+        $this->assertSame($request, $client->via($request));
+    }
+
+    /** @test */
+    public function it_can_use_different_timezone_on_request()
+    {
+        $faker = Faker::create();
+
+        Discovery::override($faker->http());
+
+        $client = Client::personal('AckfSECXIvnK5r28GVIWUAxmbBSjTsmF');
+        $client->onTimeZone('Asia/Kuala_Lumpur');
+
+        $request = m::mock('Laravie\Codex\Contracts\Request');
+        $request->shouldReceive('setClient')->once()->with($client)->andReturnNull()
+            ->shouldReceive('onTimeZone')->once()->with('Asia/Kuala_Lumpur')->andReturnNull();
+
+        $this->assertSame($request, $client->via($request));
     }
 }
